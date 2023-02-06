7ccc81f6b5e6cb42c01c8494f61840c29fde922a
Johan Löfgren <johan.lofgren@iver.se>
2023-02-06 10:24:14 +0100

    AddProject utan popup

--- src/componentsAdmin/AddProject.js
+++ src/componentsAdmin/AddProject.js
@@ -2,42 +2,56 @@ import {React, useState} from 'react'
 import { useDispatch } from 'react-redux'
 import user from '../reducers/user'
 import uniqid from 'uniqid'
+import styled from 'styled-components'
 
 const AddProject = () => {
 const dispatch = useDispatch()
 const [newName, setNewName] = useState('')
 
-const addProject = (event) => {  
+  const AddBtn = styled.button`
+  background-color: green;
+  color: #fff;
+  border-radius: 3px;
+  height: 25px;
+  width: 45px;
+  :active {
+    background-color: #4caf4c;
+  }
+  :hover {
+    box-shadow: 2px 2px 2px grey;
+  }
+`
+const addProjects = (event) => {  
     event.preventDefault()    
-    // const localStorageProjects = localStorage.getItem('user')
-    
+    // const localStorageProjects = localStorage.getItem('user')    
     const newProject = {
       // ...JSON.parse(localStorageProjects),    
       id: uniqid(),
       name: newName,        
-    }
-    
+    }    
     // localStorage.setItem('user',JSON.stringify([...action.payload])) 
     dispatch(user.actions.addProject(newProject))
-    setNewName('')  
+    setNewName('')    
 }
 
+
+
   return (
-    <div>
-      <form className='add-project' onSubmit={addProject}>       
+    <>
+      <form className='add-project' onSubmit={addProjects}> 
+        <label>Customer:</label>      
         <input
           type="text"
           value={newName}
           onChange={(event) => setNewName(event.target.value)}>
         </input>
-        <button 
-          type="submit"
-          className='btn-add'
+        <AddBtn 
+          type="submit"            
           disabled={!newName}>
             Add
-        </button>
+        </AddBtn>                
       </form>
-    </div>
+    </>
   )
 }
 
